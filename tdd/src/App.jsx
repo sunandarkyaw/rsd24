814a79b397e1cc3ea204a95c7211ccd8aa2ef94a
Su Nandar Kyaw <su.nandar.dev@gmail.com>
2024-04-01 21:53:05 +0630

    add tdd

---
+++ tdd/src/App.jsx
@@ -0,0 +1,23 @@
+import { useRef, useState } from "react";
+import { toCelsius } from "../utils/converter";
+
+export default function App() {
+    const inputRef = useRef();
+    const [result, setResult] = useState();
+
+    return (
+		<div>
+			<h1 role="title">TDD</h1>
+			<form
+				onSubmit={e => {
+					e.preventDefault();
+					const f = inputRef.current.value;
+                    setResult(toCelsius(f));
+				}}>
+				<input type="text" role="input" ref={inputRef} />
+				<button role="button">Convert</button>
+			</form>
+			{result && <div role="result">{result}</div>}
+		</div>
+	);
+}
